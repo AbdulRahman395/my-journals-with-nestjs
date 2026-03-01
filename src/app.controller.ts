2ee1e0d48e8e3ae54b263c9706c9c0ed6028429b
AbdulRahman395 <arahmanonline937@gmail.com>
2026-03-01 13:29:38 +0500

    - Heartbeat API

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,7 +1,16 @@
-import { Controller, Get, Res } from '@nestjs/common';
+import { Controller, Get, Res, HttpStatus, Req } from '@nestjs/common';
 import { AppService } from './app.service';
-import { Response } from 'express';
+import { Response, Request } from 'express';
 import { join } from 'path';
+
+interface AuthenticatedRequest extends Request {
+  user: {
+    id: string;
+    email: string;
+    isEmailVerified: boolean;
+    pinVerified: boolean;
+  };
+}
 
 @Controller()
 export class AppController {
@@ -10,5 +19,22 @@ export class AppController {
   @Get()
   getHello(@Res() res: Response) {
     res.sendFile(join(__dirname, '..', 'public', 'index.html'));
+  }
+
+  // This API is made for Frontend to send heartbeats if the user is idle but not using the app (we don't have to lock the app just as WhatsApp)
+  @Get('heartbeat')
+  heartbeat(@Req() req: AuthenticatedRequest, @Res() res: Response) {
+    // Add App Lock middleware information to headers
+    res.setHeader('X-App-Lock-Status', 'active');
+    res.setHeader('X-App-Lock-Middleware', 'applied');
+    
+    if (req.user) {
+      res.setHeader('X-User-Authenticated', 'true');
+      res.setHeader('X-User-ID', req.user.id);
+    } else {
+      res.setHeader('X-User-Authenticated', 'false');
+    }
+    
+    res.status(HttpStatus.OK).send();
   }
 }
